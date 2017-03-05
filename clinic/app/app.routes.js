2829725e05129b1679032d501f0cc65f6cda1827
leonardoward <lward.usb@gmail.com>
2017-03-05 18:42:21 -0400

    Creating Clinic Webpage

--- clinic/app/app.routes.js
+++
@@ -1,9 +0,0 @@
-import { RouterModule } from "@angular/router";
-import { PatientsComponent } from "./patients/patients.component";
-import { PATIENT_ROUTES } from "./patients/patients.routes";
-var APP_ROUTES_PROVIDERS = [
-    { path: '', redirectTo: '/patients', pathMatch: 'full' },
-    { path: 'patients', component: PatientsComponent, children: PATIENT_ROUTES },
-];
-export var routing = RouterModule.forRoot(APP_ROUTES_PROVIDERS);
-//# sourceMappingURL=../../../src/app/app.routes.js.map
